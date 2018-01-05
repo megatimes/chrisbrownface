8fd38d18b2c90f2524d44b8e75ee2beb4b9b1e9e
JB <jonathanbrockman@gmail.com>
2018-01-04 20:56:32 -0500

    Add files via upload

---
+++ chris-brown-face.js
@@ -0,0 +1,82 @@
+/* 
+ * color changing code is from 
+ * https://www.kirupa.com/html5/examples/strobe_light_generator.htm 
+ */
+
+var bodyElement = document.querySelector("body");
+var testText = document.querySelector(".test-text");
+var chrisTopSmall = document.getElementById("chris-top-small");
+var chrisTopLarge = document.getElementById("chris-top-large");
+var delay = 0;
+var delay2 = 0;
+var requestAnimationFrame = window.requestAnimationFrame || 
+                            window.mozRequestAnimationFrame || 
+                            window.webkitRequestAnimationFrame || 
+                            window.msRequestAnimationFrame;
+ 
+function changeColor() {
+    delay++;
+    delay2++;
+     
+    if (delay > 8) {
+        bodyElement.style.backgroundColor = getRandomColor();
+        testText.style.color = getRandomColor();
+        delay = 0;
+    }
+
+    if (delay <= 6) {
+        chrisTopLarge.style.bottom = '-22%';
+        chrisTopSmall.style.bottom = '-5%';
+    } else {
+        chrisTopLarge.style.bottom = '-25%';
+        chrisTopSmall.style.bottom = '-8%';
+    }
+
+    // move head from side to side
+    if (delay2 <= 40) {
+        chrisTopSmall.style.right = '0%';
+    } else if (delay2 < 80) {
+        chrisTopSmall.style.right = '-12%';
+    } else {
+        delay2 = 0;
+    }
+
+    requestAnimationFrame(changeColor);
+}              
+
+
+ 
+function getRandomColor() {
+    // creating a random number between 0 and 255
+    var r = Math.floor(Math.random()*256);
+    var g = Math.floor(Math.random()*256);
+    var b = Math.floor(Math.random()*256);
+     
+    // going from decimal to hex
+    var hexR = r.toString(16);
+    var hexG = g.toString(16);
+    var hexB = b.toString(16);
+     
+    // making sure single character values are prepended with a "0"
+    if (hexR.length == 1) {
+        hexR = "0" + hexR;
+    }
+     
+    if (hexG.length == 1) {
+        hexG = "0" + hexG;
+    }
+ 
+    if (hexB.length == 1) {
+        hexB = "0" + hexB;
+    }
+ 
+    // creating the hex value by concatenatening the string values
+    var hexColor = "#" + hexR + hexG + hexB;
+    return hexColor.toUpperCase();
+}
+
+
+
+changeColor();
+
+
